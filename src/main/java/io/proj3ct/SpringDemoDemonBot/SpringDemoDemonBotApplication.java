cb58a91293bf5e3011e270553a93eb02ae8f7fb4
Dmitry1402 <LuCI333i@mail.ru>
2023-05-09 16:40:17 +0600

    Initial commit

---
+++ src/main/java/io/proj3ct/SpringDemoDemonBot/SpringDemoDemonBotApplication.java
@@ -0,0 +1,14 @@
+package io.proj3ct.SpringDemoDemonBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoDemonBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoDemonBotApplication.class, args);
+	}
+
+}
+
